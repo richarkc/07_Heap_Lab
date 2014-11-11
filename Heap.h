8fac0954e89284e0419e733b4c1a05d5ba673c54
richarkc <richarkc@miamioh.edu>
2014-11-11 12:36:50 -0500

    Added case for growing in add and formatted code

--- Heap.h
+++ Heap.h
@@ -5,45 +5,45 @@
 #define START_SIZE 10
 
 /*
- * This is a priority queue, which means that as items are removed in order
- * of decreasing priority.
- *
- * 
- */
+* This is a priority queue, which means that as items are removed in order
+* of decreasing priority.
+*
+* 
+*/
 template<class Pri, class T>
 class Heap : public Queue<std::pair<Pri, T> > {
 public:
-  Heap();
-  ~Heap();
+	Heap();
+	~Heap();
 
-  //Add a new item
-  virtual void add(std::pair<Pri,T> toAdd);
+	//Add a new item
+	virtual void add(std::pair<Pri,T> toAdd);
 
-  //Remove the item with lowest priority, and return it
-  //If the queue is empty, throw a string exception
-  virtual std::pair<Pri,T> remove();
+	//Remove the item with lowest priority, and return it
+	//If the queue is empty, throw a string exception
+	virtual std::pair<Pri,T> remove();
 
-  //Return the number of items currently in the queue
-  virtual unsigned long getNumItems();
+	//Return the number of items currently in the queue
+	virtual unsigned long getNumItems();
 
 private:
-  int arrSize;
-  int numItems;
-  std::pair<Pri, T>* backingArray;
+	int arrSize;
+	int numItems;
+	std::pair<Pri, T>* backingArray;
 
-  //Grow the backingArray by making a new array of twice the size,
-  // and copying over the data
-  void grow();
+	//Grow the backingArray by making a new array of twice the size,
+	// and copying over the data
+	void grow();
 
-  //Check the item at index, and make sure it is in the right place.
-  // If not, swap it up the "tree" of the heap until you find the right
-  // place
-  void bubbleUp(unsigned long index);
+	//Check the item at index, and make sure it is in the right place.
+	// If not, swap it up the "tree" of the heap until you find the right
+	// place
+	void bubbleUp(unsigned long index);
 
-  //Check the item at index, and make sure it is in the right place.
-  // If not, swap it down the "tree" of the heap until you find the right
-  // place
-  void trickleDown(unsigned long index);  
+	//Check the item at index, and make sure it is in the right place.
+	// If not, swap it down the "tree" of the heap until you find the right
+	// place
+	void trickleDown(unsigned long index);  
 };
 
 //Heap.ipp file
@@ -53,9 +53,9 @@ private:
 
 template<class Pri, class T>
 Heap<Pri,T>::Heap(){
-  numItems = 0;
-  arrSize = START_SIZE;
-  backingArray = new std::pair<Pri, T> [arrSize];
+	numItems = 0;
+	arrSize = START_SIZE;
+	backingArray = new std::pair<Pri, T> [arrSize];
 }
 
 template<class Pri, class T>
@@ -75,6 +75,8 @@ void Heap<Pri,T>::grow(){
 
 template<class Pri, class T>
 void Heap<Pri,T>::add(std::pair<Pri,T> toAdd){
+	if (numItems == arrSize)
+		grow();
 	backingArray[numItems] = toAdd;
 	bubbleUp(numItems);
 	numItems++;
@@ -83,11 +85,11 @@ void Heap<Pri,T>::add(std::pair<Pri,T> toAdd){
 template<class Pri, class T>
 void Heap<Pri,T>::bubbleUp(unsigned long index){
 	int parent = (index - 1) / 2;
-  
-  if (index > 0 && (backingArray[parent].first > backingArray[index].first)) {
-	  backingArray[parent].swap(backingArray[index]);
-	  bubbleUp(parent);
-  }
+
+	if (index > 0 && (backingArray[parent].first > backingArray[index].first)) {
+		backingArray[parent].swap(backingArray[index]);
+		bubbleUp(parent);
+	}
 
 }
 
@@ -100,14 +102,14 @@ void Heap<Pri,T>::trickleDown(unsigned long index){
 	//The final case is to check when the right is a null value and the left is not, if so we want to swap, and if not, we're done and complete the method
 	if (backingArray[index] > backingArray[left] || backingArray[index] > backingArray[right]){
 		if (backingArray[left] < backingArray[right] && backingArray[left].second != "") {
-				backingArray[index].swap(backingArray[left]);
-				trickleDown(left);			
+			backingArray[index].swap(backingArray[left]);
+			trickleDown(left);			
 		}
 		else if (backingArray[right].second != "") {
-			
-				backingArray[index].swap(backingArray[right]);
-				trickleDown(right);
-			
+
+			backingArray[index].swap(backingArray[right]);
+			trickleDown(right);
+
 		}
 		else if (backingArray[left].second != "") {
 			backingArray[index].swap(backingArray[left]);
@@ -137,6 +139,6 @@ std::pair<Pri,T> Heap<Pri,T>::remove(){
 
 template<class Pri, class T>
 unsigned long Heap<Pri,T>::getNumItems(){
-  	return numItems;
+	return numItems;
 }
 
